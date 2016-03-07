563269013ed8df3228b3a30cf538f3784a403532
Sam Kreter <samkreter@gmail.com>
2016-03-07 17:08:32 -0600

    added init functin to the user model

--- app/models.py
+++ app/models.py
@@ -6,6 +6,26 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     projects = db.relationship('Project', backref='owner', lazy='dynamic')
 
+    def __init__(self, email, first_name=None, last_name=None):
+        self.email = email.lower()
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
