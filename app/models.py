44a0dbaf12ad45a195a5116d2380c4dcdef10276
Sam Kreter <samkreter@gmail.com>
2016-03-06 18:25:48 -0600

    added a test table for projects to test relationships with flask

--- app/models.py
+++ app/models.py
@@ -4,6 +4,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    projects = db.relationship('Project', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+class projects(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Project %r>' % (self.body)
