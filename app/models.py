83e3e6a6b8b3433f9d5494ef0f3a2b673b09b5dd
Sam Kreter <samkreter@gmail.com>
2016-03-23 16:05:17 -0500

    finally got the database not to freak out

--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
 from app import db,lm
 from flask.ext.login import LoginManager, UserMixin
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 
-class User(UserMixin,db.Model):
+class User(db.Model,Base,UserMixin):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -20,7 +23,7 @@ class User(UserMixin,db.Model):
 
     #relationships
     #projects = db.relationship('Project', backref='owner', lazy='dynamic')
-    companies = relationship("Company", back_populates="owner")
+    companies = db.relationship("Company", back_populates="owner")
     profile = db.relationship('Profile', uselist=False, back_populates='user')
 
     # def __init__(self, email, first_name=None, last_name=None):
@@ -51,11 +54,11 @@ class User(UserMixin,db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-class Company(UserMixin,db.Model):
+class Company(db.Model,Base):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    owner = relationship("User", back_populates="companies")
-    jobs = relationship("Job", back_populates="company")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner = db.relationship("User", back_populates="companies")
+    jobs = db.relationship("Job", back_populates="company")
     name = db.Column(db.String(120))
     #nickname = db.Column(db.String(64), index=True, unique=True)
     address = db.Column(db.String(120), index=True, unique=True)
@@ -64,44 +67,54 @@ class Company(UserMixin,db.Model):
         return '<Company %r>' % (self.name)
 
 
-profile_skill_assoc_table = Table('association', Base.metadata,
-    Column('profile_id', Integer, ForeignKey('skill.id')),
-    Column('skill_id', Integer, ForeignKey('profile.id'))
-)
+class Profile_job_assoc_table(db.Model,Base):
+    __tablename__ = 'profile_job_assoc_table'
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), primary_key=True)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), primary_key=True)
 
-profile_job_assoc_table = Table('association', Base.metadata,
-    Column('profile_id', Integer, ForeignKey('job.id')),
-    Column('job_id', Integer, ForeignKey('profile.id'))
-)
+class Profile_skill_assoc_table(db.Model,Base):
+    __tablename__ = 'profile_skill_assoc_table'
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), primary_key=True)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.id'), primary_key=True)
 
-class Skill(UserMixin,db.Model):
+
+class Job(db.Model,Base):
+    __tablename__ = 'job'
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+
+    users = db.relationship("Profile",secondary=Profile_job_assoc_table.__table__,back_populates="jobs")
+
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+    company = db.relationship("Company", back_populates="jobs")
+
+    def __repr__(self):
+        return '<Job %r>' % (self.title)
+
+
+class Profile(db.Model,Base):
+    __tablename__ = 'profile'
     id = db.Column(db.Integer, primary_key=True)
-    users = relationship("Profile",secondary=profile_skill_assoc_table,back_populates="skills")
+    resume = db.Column(db.String(120))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', back_populates='profile')
+
+    jobs = db.relationship("Job",secondary=Profile_job_assoc_table.__table__,back_populates="users")
+    skills = db.relationship("Skill",secondary=Profile_skill_assoc_table.__table__,back_populates="users")
+
+    def __repr__(self):
+        return '<Profile %r>' % (self.id)
+
+
+
+class Skill(db.Model,Base):
+    id = db.Column(db.Integer, primary_key=True)
+    users = db.relationship("Profile",secondary=Profile_skill_assoc_table.__table__,back_populates="skills")
     name = db.Column(db.String(120))
 
     def __repr__(self):
         return '<Skill %r>' % (self.name)
 
 
-class Profile(UserMixin,db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    resume = db.Column(db.String(120))
-    user = db.relationship('User', back_populates='profile')
-    jobs = relationship("Job",secondary=profile_job_assoc_table,back_populates="users")
-    skills = relationship("Skill",secondary=profile_skill_assoc_table,back_populates="users")
 
-    def __repr__(self):
-        return '<Profile %r>' % (self.id)
-
-class Job(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String(140))
-    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
-    users = relationship("Profile",secondary=profile_job_assoc_table,back_populates="skills")
-    company_id = Column(Integer, ForeignKey('company.id'))
-    company = relationship("Company", back_populates="jobs")
-
-    def __repr__(self):
-        return '<Job %r>' % (self.title)
 
