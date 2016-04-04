8b72ed60f4384fa4f5af4b3ecc869b0bcd1b3ca2
Sam Kreter <samkreter@gmail.com>
2016-04-04 11:02:21 -0500

    added bower.json

--- Kinnected-site/app/views.py
+++ Kinnected-site/app/views.py
@@ -58,6 +58,20 @@ def delete_user(id):
 def current_login():
     return "your loggin in yea"
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.get(form.email.data)
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                login_user(user, remember=True)
+                return redirect(url_for("bull.reports"))
+    return render_template("login.html", form=form)
+
 
 #to log out the current user jsut send them to this route
 @app.route('/logout')
