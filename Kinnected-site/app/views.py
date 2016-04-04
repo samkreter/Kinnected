757124d596063af13a525de6e0a1fbdeff6b3e41
Sam Kreter <samkreter@gmail.com>
2016-04-04 11:42:44 -0500

    removed the outh authentication for logings

--- Kinnected-site/app/views.py
+++ Kinnected-site/app/views.py
@@ -1,7 +1,6 @@
 from app import app
 from flask.ext.login import LoginManager, login_required, UserMixin, login_user, logout_user,current_user
 from flask import redirect, url_for, render_template,request
-from oauth import OAuthSignIn
 from sqlalchemy.exc import IntegrityError
 from app.models import User
 from app import db, lm
@@ -85,28 +84,28 @@ def unauthorized():
     # do stuff
     return redirect(url_for('index'))
 
-#userd for login to check, authenticate and create a user if nesseary
-@app.route('/authorize/<provider>')
-def oauth_authorize(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('current_login'))
-    oauth = OAuthSignIn.get_provider(provider)
-    return oauth.authorize()
+# #userd for login to check, authenticate and create a user if nesseary
+# @app.route('/authorize/<provider>')
+# def oauth_authorize(provider):
+#     if not current_user.is_anonymous:
+#         return redirect(url_for('current_login'))
+#     oauth = OAuthSignIn.get_provider(provider)
+#     return oauth.authorize()
 
-#the call back to get all of the information from the provider, probably sticking with facebook
-@app.route('/callback/<provider>')
-def oauth_callback(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('current_login'))
-    oauth = OAuthSignIn.get_provider(provider)
-    social_id, username, email = oauth.callback()
-    if social_id is None:
-        flash('Authentication failed.')
-        return redirect(url_for('index'))
-    user = User.query.filter_by(social_id=social_id).first()
-    if not user:
-        user = User(social_id=social_id, nickname=username, email=email)
-        db.session.add(user)
-        db.session.commit()
-    login_user(user, True)
-    return redirect(url_for('current_login'))
+# #the call back to get all of the information from the provider, probably sticking with facebook
+# @app.route('/callback/<provider>')
+# def oauth_callback(provider):
+#     if not current_user.is_anonymous:
+#         return redirect(url_for('current_login'))
+#     oauth = OAuthSignIn.get_provider(provider)
+#     social_id, username, email = oauth.callback()
+#     if social_id is None:
+#         flash('Authentication failed.')
+#         return redirect(url_for('index'))
+#     user = User.query.filter_by(social_id=social_id).first()
+#     if not user:
+#         user = User(social_id=social_id, nickname=username, email=email)
+#         db.session.add(user)
+#         db.session.commit()
+#     login_user(user, True)
+#     return redirect(url_for('current_login'))
